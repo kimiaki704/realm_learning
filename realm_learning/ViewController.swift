3f0508edf1c1bda763a53e96416e6fda4c9af125
kimiaki <kimioman704@gmail.com>
2019-10-30 12:04:41 +0900

    init xcode

---
+++ realm_learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realm_learning
+//
+//  Created by 鈴木公章 on 2019/10/30.
+//  Copyright © 2019 鈴木公章. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
